41cda885ed6afb538e1ce587b8a311547fe7e482
A.I <merrick@luois.me>
2017-02-17 22:58:28 +0800

    add feedparser

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [lein-sub "0.3.0"]
+                 [org.clojars.scsibug/feedparser-clj "0.5.0"]]
+  :sub ["modules/feedparser"]
   :main ^:skip-aot dmhy.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
