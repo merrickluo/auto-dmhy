93f472707397acef8a919b52bf68a6e6ff7160a4
A.I <merrick@luois.me>
2017-03-03 23:56:11 +0800

    add a timer worker

--- src/dmhy/core.clj
+++ src/dmhy/core.clj
@@ -5,7 +5,8 @@
             [compojure.route :as route]
             [clojure.data.json :as json]
             [monger.core :as mg]
-            [monger.collection :as mc]))
+            [monger.collection :as mc]
+            [clojure.core.async :as async :refer [<! timeout chan go]]))
 
 (def db
   (let [conn (mg/connect)]
@@ -17,7 +18,8 @@
   "change settings"
   [req]
   (doseq [[path value] (map identity req)]
-    (mc/update db "settings" {:path path} {:path path :value value} {:upsert true})))
+    (mc/update db "settings" {:path path} {:path path :value value} {:upsert true}))
+  "ok")
 
 (defn add-feed
   "add dmhy rss feed"
@@ -38,3 +40,29 @@
   (POST "/settings" {body :body} (settings (body2json body)))
   (POST "/feed" {body :body} (add-feed (body2json body)))
   (route/not-found "<h1>Page not found</h1>"))
+
+;; parser rss and send download to aria
+(def interval (atom 3000))
+(def should-work (atom true))
+
+(defn query-interval []
+  (let* [setting (mc/find-one-as-map db "settings" {:path "interval"})
+         new-interval (if (nil? setting) 3000 (read-string (:value setting)))]
+    (if (not= new-interval interval)
+      (reset! interval new-interval))))
+
+(defn start-work []
+  (reset! working true))
+
+(defn stop-work []
+  (reset! working false))
+
+(defn work []
+  (println "working"))
+
+(go
+  (while true
+    (query-interval)
+    (<! (timeout @interval))
+    (when @should-work
+      (work))))
