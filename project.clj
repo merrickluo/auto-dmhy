ac0163509cfe3dcc4ebf122bbe7b962ed918b432
A.I <merrick@luois.me>
2017-03-03 23:25:47 +0800

    add api to set ariarpc path and add feed

--- project.clj
+++ project.clj
@@ -5,8 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [lein-sub "0.3.0"]
+                 [compojure "1.5.2"]
+                 [org.clojure/data.json "0.2.6"]
+                 [com.novemberain/monger "3.1.0"]
                  [org.clojars.scsibug/feedparser-clj "0.5.0"]]
+  :plugins [[lein-ring "0.11.0"]]
   :sub ["modules/feedparser"]
   :main ^:skip-aot dmhy.core
   :target-path "target/%s"
+  :ring {:handler dmhy.core/app}
   :profiles {:uberjar {:aot :all}})
--- src/dmhy/core.clj
+++ src/dmhy/core.clj
@@ -1,7 +1,40 @@
 (ns dmhy.core
-  (:gen-class))
+  (:gen-class)
+  (:require [feedparser-clj.core :as parser]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [clojure.data.json :as json]
+            [monger.core :as mg]
+            [monger.collection :as mc]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def db
+  (let [conn (mg/connect)]
+    (mg/get-db conn "dmhy")))
+
+(map println (take 1 (:entries (parser/parse-feed "http://share.dmhy.org/topics/rss/rss.xml"))))
+
+(defn settings
+  "change settings"
+  [req]
+  (doseq [[path value] (map identity req)]
+    (mc/update db "settings" {:path path} {:path path :value value} {:upsert true})))
+
+(defn add-feed
+  "add dmhy rss feed"
+  [req]
+  (prn req)
+  (let [url (get req "url")]
+    (mc/update db "feeds" {:url url} {:url url} {:upsert true}))
+  "ok")
+
+(mc/find-maps db "settings" {:path "ariarpc"})
+(defn body2json
+  "parse request body to json"
+  [body]
+  (json/read-str (slurp body)))
+
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (POST "/settings" {body :body} (settings (body2json body)))
+  (POST "/feed" {body :body} (add-feed (body2json body)))
+  (route/not-found "<h1>Page not found</h1>"))
