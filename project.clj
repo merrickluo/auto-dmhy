d62138167dd74e0a94066377dfbd7f7d7828de8f
A.I <merrick@luois.me>
2017-03-10 16:16:35 +0800

    fix plugin position

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.3.0"]
                  [org.clojure/core.async "0.3.441"]
-                 [lein-sub "0.3.0"]
                  [compojure "1.5.2"]
                  [org.clojure/data.json "0.2.6"]
                  [com.novemberain/monger "3.1.0"]
                  [org.clojars.scsibug/feedparser-clj "0.5.0"]]
-  :plugins [[lein-ring "0.11.0"]]
+  :plugins [[lein-ring "0.11.0"]
+            [lein-sub "0.3.0"]]
   :sub ["modules/feedparser"]
   :main ^:skip-aot dmhy.core
   :target-path "target/%s"
